fc7c7deac1169bc9529b4e6d7d28cde143e2ad8e
yoonipark <yoonipark09@gmail.com>
2024-05-01 15:14:28 +0900

    TEST

--- src/main/java/org/example/aws_server_test/test/testController.java
+++ src/main/java/org/example/aws_server_test/test/testController.java
@@ -1,1 +1,17 @@
-package org.example.aws_server_test.test;public class testController {}
+package org.example.aws_server_test.test;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class testController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World! :)";
+    }
+}
